e68620c8ec8bf2de77e43680fa43d40c08f666fa
smartwalle <smartwalle@gmail.com>
2023-06-12 08:47:42 +0800

    目录调整

--- examples/server1.go
+++ examples/server1.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	var s = gin.Default()
 	s.GET("/test", func(c *gin.Context) {
-		c.Request.ParseForm()
 		c.Writer.WriteString("9910")
-		fmt.Println(c.Request.Form)
 	})
 	s.GET("/test/h", func(c *gin.Context) {
 		c.Writer.WriteString("h")
---
+++ examples/server2.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	var s = gin.Default()
+	s.GET("/test", func(c *gin.Context) {
+		c.Writer.WriteString("9911")
+	})
+	s.GET("/test/h", func(c *gin.Context) {
+		c.Writer.WriteString("h")
+	})
+	s.Run(":9911")
+}
